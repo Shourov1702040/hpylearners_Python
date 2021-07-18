0a13f571d831dd98dd0dd84a748ba0b943b521a6
Md Touhid Islam <36205091+Shourov1702040@users.noreply.github.com>
2021-07-18 11:37:13 +0600

    Add files via upload

---
+++ digital watch.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from time import strftime
+
+def time():
+    string = strftime('%I:%M:%S %p')
+    label.config(text=string)
+    label.after(1000,time)
+
+root = Tk()
+
+label = Label(root,font = "Digital-7 25 bold",fg='green')
+label.pack(pady=20)
+time()
+
+root.geometry('300x120+400+300')
+root.mainloop()
