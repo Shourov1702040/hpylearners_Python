d2d874f03ee521584dde6576fed062a122329bac
Md Touhid Islam <36205091+Shourov1702040@users.noreply.github.com>
2021-08-30 22:01:54 +0600

    Update 01.py

--- 01.py
+++ 01.py
@@ -1,7 +1,18 @@
+# Python program to display the Fibonacci sequence
 
-print("You see that? \n how ease python is.")
+def recur_fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(recur_fibo(n-1) + recur_fibo(n-2))
 
+nterms = 10
 
-print("Just one line to print!!!!!!!!!")
-
+# check if the number of terms is valid
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(recur_fibo(i))
 
