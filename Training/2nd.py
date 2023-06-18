0f44fbe194d00e8cc1d63e2f113ecfdeabc685c1
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-06-18 15:53:11 +0600

    Add files via upload

---
+++ Training/2nd.py
@@ -0,0 +1,85 @@
+# Python Comments 
+
+"""Python Comments 1
+ Python Comments 2 fjlskdf8 sdfihk skjdh k
+ Python Comments 3"""
+
+# print("Python programming") # printing a string
+
+# Python Variables 
+
+# Rules-----
+
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords. 
+
+# name3= "Shuvo"
+
+# print(name3) 
+
+# Python Variables Value assign: 
+# Assign Multiple Values
+
+# var1 = 3.14
+# var2 = 1.212
+# var3 = 2.588
+
+var1, var2, var3 = 3.14, 1.212, 2.588
+
+var4 = var5 = var6 = 3.14
+
+# print("var6") 
+
+# name1 = 'Toufiq'
+# name2 = "Shuvo"
+
+# num1 = "5"
+# num2 = "6"
+
+# age = 17
+
+# print("Resule is : ",num1+num2) 
+
+# Local and Global 
+
+
+# def function_1(): 
+#     new_var = 34
+
+# new_var = 462
+# print(new_var)
+
+
+# String
+
+st1 = "Python"
+st2 = "Programming"
+
+st3 = st1 + " " + st2 + ' "languages"'
+
+print(st3) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
