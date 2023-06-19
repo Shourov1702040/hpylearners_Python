1ca537891e6138d0b1f58a6ed92728f1f8dbf9ed
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-06-19 20:42:54 +0600

    Add files via upload

---
+++ Training/3rd.py
@@ -0,0 +1,93 @@
+# Text = "fhksj", 'd' -> string
+str_1 = "This is a string."
+# print(str_1)
+
+# Numeric Types: int, float, complex = x+yj 
+x1 = 1
+x2 = 2
+x3 = 3
+x4 = 4
+x5 = 5.68
+
+# print(x2)
+
+# Sequence/Collection Types: list, tuple, range 
+# 1  0
+# 2  1
+# 3  2
+# ..
+
+numbers = [1, 2, 3, 2, 5, 28, 0] 
+
+# indexing
+# print(type(numbers))
+
+
+# ascending, descending
+# Set
+set_1 = { 2, 5, 28, 0, 1, 2, 3}
+# print(type(set_1)) 
+
+# type casting
+
+new_x = int(x5)
+new_x2 = float(x4)
+
+# print(type(x5))
+# print(type(new_x))
+# print(type(x4))
+# print(type(new_x2))
+
+numbers_set = set(numbers) 
+
+# print(type(numbers_set))
+# print(numbers_set)
+
+
+num_str = "76564"
+n = int(num_str) 
+# print(type(n))
+
+
+
+# Numbers 
+# int, float, complex
+
+x = 2
+y = 2.53
+z = complex(2+6j)
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(z)
+
+# ENG 2.0*10^5
+
+# 3^5 
+x1 = 3.0**5
+x2 = 3e5
+x3 = 2.34E5
+
+# print(type(x1))
+
+# Random number 
+import random
+0,1,2,3,4,5,6,7,8,9,10
+rand = random.randrange(1, 11)
+
+
+print(rand)
+
+
+# Homework
+# Enter you rname: Toufiq
+# Tickte_id : 23
+# Your score = 36%
+
+
+
+
+
+
+
