1641f712dfaa2b60747083f2353ef6caedf711d2
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-06-24 21:44:33 +0600

    Add files via upload

---
+++ Training/4class.py
@@ -0,0 +1,38 @@
+x = 12
+y = 21
+z = "12"
+print(x>=y)
+
+# n = 132342%13
+# print(n)
+# print(x>y or x<z)
+# print(not(x>y or x<z))
+
+# lst1 = [1,2,3]
+# lst2 = [2,3,4]
+# k = 4
+# # print(k not in lst1)
+
+
+# # print(x is not lst1)
+
+# n = int(input("Enter your age: "))
+
+# if n>20:
+#     print("Welcome")
+#     print("You are a pro player")
+
+# elif n>15:
+#     print("Welcome")
+#     print("You are elligible")
+
+# elif n>10:
+#     print("Welcome")
+#     print("You are elligible")
+# else:
+#     print("You are not elligible")
+
+
+
+
+
---
+++ Training/5_class.py
@@ -0,0 +1,70 @@
+# if condition(Returns Binary):
+#     # body
+
+# if condition2:
+#     # body
+
+age = 15
+name1 = 'Toufiq'
+name2 = 'Shuvo'
+
+# if age>=17:
+#     print("Welcome, You can enter")
+# else:
+#     print("Sorry, you can not enter")
+
+# if name1=='Toufiq' and name2 == 'Shuvo':
+#     print("You can join the class")
+
+# print(age>=17)
+
+
+
+# Else if 
+
+# mark = int(input("Enter your mark: "))
+# # mark2 = int(input("Enter your mark: "))
+# # mark3 = int(input("Enter your mark: "))
+# gread = None
+
+# if mark >= 80:
+#     print("A+")
+#     gread=5.0
+
+# elif mark >= 70:
+#     print("A")
+#     gread=4.0
+
+# elif mark >= 60:
+#     print("A-")
+#     gread=3.5
+
+# elif mark >= 50:
+#     print("B")
+#     gread=3.0
+
+# else:
+#     print("Fail")
+#     gread=0.0
+
+# print("Your result = ",gread)
+
+lst = [1, 2, 3, 4, 5]
+
+# String 
+
+string_1 = "Python programming"
+text = """A short poem may be 
+        a stylistic choice or 
+        it may be that you have said 
+        what you intended to say in a 
+        more concise way. Either way, they 
+        differ stylistically from a long poem
+        in that there tends to be more care 
+        in word choice."""
+
+# text_2 = string_1+" "+text
+# print(text_2)
+
+print(string_1.lower())
+
