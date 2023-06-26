211837070c2cd5420019d52c5ef9e972e5b65b44
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-06-26 22:20:33 +0600

    Add files via upload

---
+++ Training/6_class.py
@@ -0,0 +1,55 @@
+string_1 = "    Python*programming*language   "
+
+
+# string_2 = string_1.lstrip()
+# string_2 = string_1.rstrip()
+string_2 = string_1.strip()
+
+# print(string_1.upper())
+# print(string_1.lower())
+
+# print(string_1)
+# print(string_1.strip())
+# print(string_2)
+
+
+string_3 = string_2.replace("P", "L")
+# print(string_3)
+
+# print("Total number of caracter = ",string_2.count('n'))
+
+string_4 = "sTRING"
+string_5 = "7542"
+
+# print(string_4.isalpha())
+# print(string_5.isalpha())
+
+
+# print(string_4.isdigit())
+# print(string_5.isdigit())
+
+
+# print(string_2.split('*'))
+
+
+# print(string_4.swapcase())
+
+
+# String formating, 
+age = 17
+# string_6 = "I am "+17+" years old"
+
+# string_6 = f'My name is Mr. X, I am {age} year\'s old'
+# string_6 = f'My name is Mr. X,\nI am {age} years old'
+string_6 = f'My name is Mr. X,\tI am {age} years old'
+print(string_6)
+
+
+
+
+
+
+
+
+
+
