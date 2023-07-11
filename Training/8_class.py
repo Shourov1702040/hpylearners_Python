aba8e90f8333c29740e625f1ff6d4b7c064a7be2
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-07-11 15:29:50 +0600

    Add files via upload

---
+++ Training/8_class.py
@@ -0,0 +1,55 @@
+# n = int(input("Enter the last roll: "))
+
+# i=0
+
+# while i<101:
+#     print(f"Roll no. {i}")
+    
+#     if i==n:
+#         break
+#     i += 1
+
+
+# while i<11:
+#     print(i)
+#     i+=1
+
+# print("\n\n----------------------------------------------------\n\n")
+
+# i=1
+# while i<11:
+#     print(i)
+#     i=+1
+# i +=1 # i = i+1
+
+# i =+1 # i = 1 
+
+
+# while i<100:
+
+#     i+=1
+#     if i>50 and i<71:
+#         print(f"Roll no. {i} is absent")
+#         continue
+
+#     print(f"Roll no. {i}")
+
+# roll = [1,2,3,4,5,6,7,8,9,10]
+# roll = (1,2, 653,64, 64,482,3254, 75)
+# string = "ABCDEFGH"
+
+# for i in string:
+#     print(i)
+
+classes = [1,2,3,4,5,6,7,8,9,10]
+rolls = [1,3,5,7]
+
+for i in classes:
+    for j in rolls:
+        # print(f"Class {i}, roll no. {j}")
+        # pass 
+
+
+    
+
+
---
+++ Training/Loop.py
@@ -0,0 +1,13 @@
+S = "Python programming"
+E = int(input("Enter a number, up to which you want to see the list ="))
+R = E+1
+Z = 1
+
+while Z<R:
+    print(f'{Z}. {S}')
+    Z += 1
+
+# h = f" The is a string containing {S}"   
+# h = " The is a {} string containing {}".format(Z, S)    
+
+    
