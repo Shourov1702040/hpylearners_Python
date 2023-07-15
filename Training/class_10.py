91ba39fb665a12c5998ae4106b5f91abe862b104
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-07-15 18:18:37 +0600

    Add files via upload

---
+++ Training/class_10.py
@@ -0,0 +1,60 @@
+# Remove items from list
+
+lst1 = ['C', 'C++', 'Java', 'Python', 'Ruby', 'C++', 'Java']
+
+lst3 = [True, False, False, True]
+lst4 = ['C++', 101, True, 3.1416]
+
+# lst1.remove('Python')
+# print(lst1)
+
+# lst1.pop()
+# lst1.pop(-1)
+
+
+# del lst1[-1]
+
+# del lst1
+
+# lst1.clear()
+# print(lst1)
+
+# for item in lst1:
+#     print(item)
+
+# new_list = []
+# Comprehension
+# for item in lst1:
+#     if 'C' in item:
+#         new_list.append(item)
+
+# print(new_list)
+
+
+# new_list = [item for item in lst1 if 'C' in item]
+# print(new_list)
+
+
+
+lst2 = [2,3,68,9,34,2]
+# lst2_new = []
+
+# # lst2_new = lst2
+# lst2_new = lst2.copy()
+
+# print(lst2_new)
+
+
+
+# sort 
+
+# lst2.sort()
+# print(lst2)
+
+# lst3.sort()
+# print(lst3)
+
+# lst1.sort()
+# print(lst1)
+
+
