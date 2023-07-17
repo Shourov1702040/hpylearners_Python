405626f2f512c350ef3b1af1ed3c609f6fddf640
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-07-17 21:01:10 +0600

    Add files via upload

---
+++ Training/class_11.py
@@ -0,0 +1,45 @@
+tpl1 = 'C', 'C++', 'Java', 'Python', 'Ruby', 'C++', 'Java'
+tpl3 = (True, False, False, True)
+
+
+# print(type(tpl1))
+
+# print(len(tpl1))
+
+# print(tpl1[6])
+
+# print(tpl1)
+
+# x, y, z = input("Enter 3 values separated by comma:" ).split("@")
+# t = x, y, z
+
+# c = 324
+# print(t)
+
+# for item in tpl1:
+#     print(item)
+
+
+# Update tuple 1
+# tpl_new = list(tpl1)
+# tpl_new.append("Go")
+# tpl_new = tuple(tpl_new)
+
+# print(tpl_new)
+
+# Update tuple 2
+
+# tpl4 = ('Go',) 
+
+# tpl1 += tpl4
+
+# print(tpl1)
+
+
+tpl4 = ('C++', 101, True, 3.1416)
+
+# x,y,z,k = tpl4
+
+# print(x,y,z,k)
+
+print(tpl1.count("C++"))
