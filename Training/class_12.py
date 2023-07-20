f1cf09d61998fe05ad306b6f4ce4f054373a99ef
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-07-20 10:35:31 +0600

    Add files via upload

---
+++ Training/class_12.py
@@ -0,0 +1,94 @@
+# tpl = (1,2,5,3,9,6,8,4,10,7)
+
+# sum = 0
+
+# for i in tpl:
+#     # sum = sum+i 
+#     sum +=i
+
+# print("The sum of elements= ",sum)
+
+
+# tpl = ('C', 101, 'C++', 'Java', 3.1416, True, 'Python', False)
+
+# count = 0
+
+# for item in tpl:
+#     t = str(type(item))
+#     t = t.split("'")[1]
+#     if t=='str':
+#         count += 1
+
+# print("Total number of string= ",count)
+
+# # class'str'
+
+# ['class', 'str', '']
+
+# x,y,z = input("Enter values comma separated: ").split(',')
+
+# # print(x)
+
+# # lst1 = []
+# # n=4
+# # for i in range(0,n):
+# #     x = input(f"Enter number {i+1}: ")
+# #     # lst1.append(x)
+# #     lst1.insert(0, x)
+
+# # print(lst1[-1::-1])
+# print(x,y,z)
+
+
+# tpl = ['C', 101, 'C++', 'Java', 3.1416, True, 'Python', False]
+
+# print(tpl.index(True))
+
+
+# n = range(0,4)
+
+# print(n)
+
+
+set1 = {'C', 'C++', 'Java', 'Python', 'Ruby', 'C++', 'Java'}
+set2 = {'C', 101, 'C++', 'Java', 3.1416, True, 'Python', False}
+
+# print(type(set2))
+# print(set2)
+
+# print(len(set2))
+
+# for i in set1:
+#     print(i)
+
+# print('PHP' in set1)
+# 
+# set1.add('PHP') 
+# print(set1)
+
+# set3 = {'PHP','Javascript'}
+
+# set1.update(set3)
+
+# print(set1)
+
+# set1.remove('Ruby')
+set1.pop()
+
+# set1.discard('Ruby')
+print(set1)
+
+set3 = {1,2,4,5,7}
+set4 = {3,5,6,7,8}
+
+# set_new = set3.intersection(set4)
+# print(set_new)
+
+# set_new = set3.union(set4)
+# print(set_new)
+
+
+
+
+
+
