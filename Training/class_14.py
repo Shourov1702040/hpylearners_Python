60ee2ed18d8b026e9f2b595898bd386d074bd7ed
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-07-31 20:28:09 +0600

    Add files via upload

---
+++ Training/class_14.py
@@ -0,0 +1,50 @@
+dct = {1:"C", 2:"Java", 3:"GO", 4:"PHP"}
+
+# dct.update({3:"Python"})
+
+# dct[3]="Python"
+
+
+# dct.update({5:"Python"})
+
+dct[5]="Python"
+
+# dct.pop(5)
+# dct.popitem()
+
+# del dct[5]
+
+# del dct
+# dct.clear()
+
+# print(dct)
+# print(dct[3])
+
+# for i in dct:
+#     print(i)
+#     print(dct[i])
+
+# for i in dct.keys():
+#     print(i)
+
+# for i in dct.values():
+#     print(i)
+
+# for i,j in dct.items():
+#     print(i,": ",j)
+# dct2 = dct
+
+# dct2 = dct.copy()
+
+# print(dct2)
+
+# dct[1] = {'I':'C', 'II':'C++', 'III':'C#'}
+
+# print(dct)
+
+# input: 
+# Enter a value: 3
+# Enter a operator: -
+# Enter another number: 2
+
+# Output= 1
