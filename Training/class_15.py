bc70bf58b816d24fe681804c5e6adf44a1d373a3
Md. Touhid Islam Shourov <shourovhstu17@gmail.com>
2023-08-03 18:02:09 +0600

    Add files via upload

---
+++ Training/class_15.py
@@ -0,0 +1,39 @@
+# Iterator 
+tpl = ('Mango', 'Lichi', 'Jack_fruit', 'Banana')
+lst = [1,2,3,4,5]
+
+itr = iter(tpl)
+
+# print(type(itr))
+st = next(itr)
+
+# print(next(itr))
+
+s = 10
+# function 
+
+def toufiq_addition(a, b, c):
+    c = a+b+c
+    # c = c+s
+    # print("Sum of a and b = ",c)
+    return c
+
+
+# print(c)
+result = toufiq_addition(2,4,6)
+print(result) 
+
+
+
+s = "       Toufiq...ur........     Ran...man"
+
+result = fucn(s)
+print(result)
+
+# output:
+#  Toufiqur Ranman
+
+
+
+
+
